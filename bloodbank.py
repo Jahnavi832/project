fcd221acc29dec2bf84b6d66301fc217c9ee203c
Jahnavi832 <324103211038.jahnavi@gvpcew.ac.in>
2026-01-28 19:09:39 +0530

    Delete bloodbank.py

--- bloodbank.py
+++
@@ -1,8 +0,0 @@
-import webbrowser
-
-def find_blood_banks():
-    search_query = "https://www.google.com/maps/search/blood+bank+near+me"
-    webbrowser.open(search_query)
-    print("Redirecting to Google Maps to show nearby blood banks...")
-
-find_blood_banks()
